265292bd4a0e50cc32219a79850c731d729b5c31
Your Name <you@example.com>
2023-12-30 21:42:28 +0200

    Email send

--- resources/views/mail/welcome.blade.php
+++ resources/views/mail/welcome.blade.php
@@ -10,6 +10,5 @@
     <p style="font-size: 16px;">Это текст вашего сообщения. Дополнительные стили применяются здесь.</p>
     <p style="font-size: 14px; font-style: italic;">{{ $body }}</p>
 </div>
-
 </body>
 </html>
