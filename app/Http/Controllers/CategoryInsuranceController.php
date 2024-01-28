9cfcc478bbac7e573c7b93274124426b1550b0ce
Your Name <you@example.com>
2024-01-29 00:35:33 +0200

    add seeds
    Insurance

---
+++ app/Http/Controllers/CategoryInsuranceController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category\CategoryInsurance;
+use Illuminate\Http\Request;
+
+class CategoryInsuranceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(CategoryInsurance $categoryInsurance)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(CategoryInsurance $categoryInsurance)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, CategoryInsurance $categoryInsurance)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(CategoryInsurance $categoryInsurance)
+    {
+        //
+    }
+}
---
+++ database/factories/Category/CategoryInsuranceFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories\Category;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Category\CategoryInsurance>
+ */
+class CategoryInsuranceFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->name,
+            'phones' => json_encode(getRandomPhone()),
+            'email' => $this->faker->unique()->safeEmail,
+            'logo' => $this->faker->imageUrl,
+            'address' => $this->faker->address,
+            'director' => $this->faker->name,
+        ];
+    }
+}
