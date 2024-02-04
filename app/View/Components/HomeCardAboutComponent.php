88b4239c21df22810756b53306ac52364b4830f6
Your Name <you@example.com>
2024-02-05 01:41:41 +0200

    payment

--- app/View/Components/HomeCardAboutComponent.php
+++ app/View/Components/HomeCardAboutComponent.php
@@ -21,7 +21,6 @@ class HomeCardAboutComponent extends Component
      */
     public function render(): View|Closure|string
     {
-        dd($this->data);
         return view('components.home.card-about-component')
             ->with('card_data', $this->data);
     }
