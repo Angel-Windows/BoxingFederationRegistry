3f7e5d77d9b83c56a7484347ced3fea3fb21a0b8
Your Name <you@example.com>
2024-02-05 01:24:56 +0200

    payment

--- app/Http/Controllers/Page/HomeController.php
+++ app/Http/Controllers/Page/HomeController.php
@@ -12,6 +12,7 @@ class HomeController extends Controller
     public function index()
     {
         $card_data = ClassType::all();
+        dd($card_data);
         return view('page.home', compact('card_data'));
     }
 }
