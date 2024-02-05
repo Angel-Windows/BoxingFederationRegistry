d906255c30d813374e46d9261a7d286a12dc9578
Your Name <you@example.com>
2024-02-05 12:11:55 +0200

    payment

--- app/Http/Controllers/ConfigController.php
+++ app/Http/Controllers/ConfigController.php
@@ -8,6 +8,7 @@ class ConfigController extends Controller
 {
     public function showImg(Request $request): \Symfony\Component\HttpFoundation\BinaryFileResponse
     {
+        dump(2);
         $filename = $request->input('filename');
         $path = storage_path('app/' . $filename); // Путь к файлу в хранилище
         if (!\Storage::exists($filename)) {
