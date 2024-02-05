2aa3ef08c7081af31217eb6a127bf1dd01976857
Your Name <you@example.com>
2024-02-05 02:55:06 +0200

    payment

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -7,8 +7,11 @@ use Carbon\Carbon;
 use Cloudipsp\Checkout;
 use Cloudipsp\Exception\ApiException;
 use Cloudipsp\P2pcredit;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Cloudipsp\Configuration;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
 class PaymentController extends Controller
 {
@@ -27,28 +30,32 @@ class PaymentController extends Controller
         return self::fondyBuy(1, 'eliphas.sn@gmail.com');
     }
 
-    public function response_url(Request $request)
+    public function response_url(Request $request): void
+    {
+        $merchant_data = json_decode($request->input('merchant_data'), false, 512, JSON_THROW_ON_ERROR);
+        redirect()->route('page.class')
+            ->with('class_name', $merchant_data->type)
+            ->with('id', $merchant_data->id);
+    }
+
+    public function callback_url(Request $request): void
     {
         $merchant_data = json_decode($request->input('merchant_data'), false, 512, JSON_THROW_ON_ERROR);
         $order_time = $request->input('order_time');
-        \DB::table($merchant_data->type)
+        DB::table($merchant_data->type)
             ->where('id', $merchant_data->id)
             ->update(['end_subscription' => Carbon::parse($order_time)->addYear()]);
-        dd($merchant_data);
-//        order_time
-//        actual_amount == 100
-//        response_status == 'success'
-//        order_id
-//        payment_id
-//        product_id
-    }
-    public function callback_url(Request $request){
-//        $model  = new \App\Models\Qualification();
-//        $model->name =  json_encode($request->input() ?? '');
-//        $model->save();
-
-        \Log::info( json_encode($request->input() ?? ''));
-        \Log::info( $request->input());
-
+        Log::info(json_encode([
+            'type' => 'transaction',
+            [
+                'payment' => 'fondy',
+                'product_id' => $request->input('product_id'),
+                'payment_id' => $request->input('payment_id'),
+                'order_id' => $request->input('order_id'),
+                'actual_amount' => $request->input('actual_amount'),
+                'response_status' => $request->input('response_status'),
+                'order_time' => $request->input('order_time'),
+            ]
+        ], JSON_THROW_ON_ERROR));
     }
 }
