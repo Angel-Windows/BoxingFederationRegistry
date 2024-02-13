530bf38689069dfc83aaeb67939ec853f3a925e2
Your Name <you@example.com>
2024-02-13 05:05:25 +0200

    Employess fool register

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -33,6 +33,16 @@ class PaymentController extends Controller
 
     public function response_url(Request $request): \Illuminate\Http\RedirectResponse
     {
+        $now = Carbon::now();
+        $merchant_data = json_decode($request->input('merchant_data'), false, 512, JSON_THROW_ON_ERROR);
+
+        $order_time = $request->input('order_time');
+        $transaction = TransactionCategory::where('key', $merchant_data->key)->first();
+        $transaction->update(['status' => 2, 'get_transaction_at'=> $now]);
+
+
+
+
         $merchant_data = json_decode($request->input('merchant_data'), false, 512, JSON_THROW_ON_ERROR);
         return redirect()->route('page.class', [
             'class_name' => $merchant_data->type,
@@ -43,26 +53,26 @@ class PaymentController extends Controller
     public function callback_url(Request $request): void
     {
 
-        $now = Carbon::now();
-        $merchant_data = json_decode($request->input('merchant_data'), false, 512, JSON_THROW_ON_ERROR);
-
-        $order_time = $request->input('order_time');
-        $transaction = TransactionCategory::where('key', $merchant_data->key)->first();
-        $transaction->update(['status' => 2, 'get_transaction_at'=> $now]);
-
-        Log::info(json_encode([
-            'type' => 'transaction',
-            [
-                'payment' => 'fondy',
-                'product_id' => $request->input('product_id'),
-                'payment_id' => $request->input('payment_id'),
-                'merchant_key' => $merchant_data->key,
-                'order_id' => $request->input('order_id'),
-                'actual_amount' => $request->input('actual_amount'),
-                'response_status' => $request->input('response_status'),
-                'order_time' => $request->input('order_time'),
-                'time' => $now,
-            ]
-        ], JSON_THROW_ON_ERROR));
+//        $now = Carbon::now();
+//        $merchant_data = json_decode($request->input('merchant_data'), false, 512, JSON_THROW_ON_ERROR);
+//
+//        $order_time = $request->input('order_time');
+//        $transaction = TransactionCategory::where('key', $merchant_data->key)->first();
+//        $transaction->update(['status' => 2, 'get_transaction_at'=> $now]);
+//
+//        Log::info(json_encode([
+//            'type' => 'transaction',
+//            [
+//                'payment' => 'fondy',
+//                'product_id' => $request->input('product_id'),
+//                'payment_id' => $request->input('payment_id'),
+//                'merchant_key' => $merchant_data->key,
+//                'order_id' => $request->input('order_id'),
+//                'actual_amount' => $request->input('actual_amount'),
+//                'response_status' => $request->input('response_status'),
+//                'order_time' => $request->input('order_time'),
+//                'time' => $now,
+//            ]
+//        ], JSON_THROW_ON_ERROR));
     }
 }
