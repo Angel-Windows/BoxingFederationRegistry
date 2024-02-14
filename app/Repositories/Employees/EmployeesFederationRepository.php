9d1fa47ee7a3e79a6d255900b33902da09be1aa2
Your Name <you@example.com>
2024-02-14 15:53:38 +0200

    modal fix

--- app/Repositories/Employees/EmployeesFederationRepository.php
+++ app/Repositories/Employees/EmployeesFederationRepository.php
@@ -73,7 +73,7 @@ class EmployeesFederationRepository implements CategoryRepositoryInterface
     public function edit($id, $request, $type): array
     {
         $category = self::validate_category($request, $this->table_model, $id);
-
+        dd($request->input('position') ?? '');
         $category->federation_id = $request->input('federation') ?? '';
         $category->name = $request->input('name') ?? '';
         $category->phone = $request->input('phone') ?? '';
