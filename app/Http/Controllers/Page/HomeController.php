8150d342f76a352c79dfe6ac76852e41d68e3040
Your Name <you@example.com>
2024-02-14 15:48:46 +0200

    modal fix

--- app/Http/Controllers/Page/HomeController.php
+++ app/Http/Controllers/Page/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Page;
 
 use App\Http\Controllers\Controller;
 use App\Models\Class\ClassType;
+use App\Services\SmsService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Storage;
@@ -12,7 +13,7 @@ class HomeController extends Controller
 {
     public function index()
     {
-
+        SmsService::sendSms('+380956686191', 'test');
         $card_data = ClassType::whereIsset('description');
         return view('page.home', compact('card_data'));
     }
--- app/Services/SmsService.php
+++ app/Services/SmsService.php
@@ -7,10 +7,39 @@ use Infobip\Model\SmsAdvancedTextualRequest;
 use Infobip\Model\SmsDestination;
 use Infobip\Model\SmsTextualMessage;
 use Infobip\Configuration;
-
+use Illuminate\Support\Facades\Http;
 class SmsService
 {
     public static function sendSms($phoneNumber, $message): \Exception|\Infobip\Model\SmsResponse|\Infobip\Model\ApiException|ApiException|null
+    {
+        $response = Http::get('https://api.turbosms.ua/message/send.json', [
+            'token' => 'fc4123507284046d5222ddd4ef87a83445b584e4',
+            'recipients' => [
+                '380956686191'
+            ],
+            'sms' => [
+                'sender' => 'TurboSMS',
+                'text' => 'TurboSMS вітає Вас!'
+            ],
+
+            'REQUIRED_TOKEN' => 'fc4123507284046d5222ddd4ef87a83445b584e4',
+            'TOKEN' => 'fc4123507284046d5222ddd4ef87a83445b584e4',
+            // Убедитесь, что это ваш действительный токен
+        ]);
+
+        $body = $response->body();
+
+        $status = $response->status();
+
+        $headers = $response->headers();
+
+//        https://api.turbosms.ua/message/send.json?recipients[0]=380956686191&sms[sender]=TurboSMS&sms[text]=TurboSMS+%D0%B2%D1%96%D1%82%D0%B0%D1%94+%D0%92%D0%B0%D1%81%21&token=fc4123507284046d5222ddd4ef87a83445b584e4
+//        https://api.turbosms.ua/message/send.json?recipients[0]=380678998668&recipients[1]=380503288668&recipients[2]=380638998668&sms[sender]=TurboSMS&sms[text]=TurboSMS+%D0%B2%D1%96%D1%82%D0%B0%D1%94+%D0%92%D0%B0%D1%81%21&token=AUTH_TOKEN
+
+        dd($response,$body, $status, $headers);
+    }
+
+    public static function infobipSendSms($phoneNumber, $message): \Exception|\Infobip\Model\SmsResponse|\Infobip\Model\ApiException|ApiException|null
     {
         $configuration = new Configuration(
             host: 'https://qyqprm.api.infobip.com',
@@ -34,4 +63,5 @@ class SmsService
             // HANDLE THE EXCEPTION
         }
     }
+
 }
