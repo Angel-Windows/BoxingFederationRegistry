dc0e486987a9447a3f7690a4aa3fa7fc48b13249
Your Name <you@example.com>
2024-02-14 15:49:24 +0200

    modal fix

--- app/Http/Controllers/Page/HomeController.php
+++ app/Http/Controllers/Page/HomeController.php
@@ -13,7 +13,7 @@ class HomeController extends Controller
 {
     public function index()
     {
-        SmsService::sendSms('+380956686191', 'test');
+//        SmsService::sendSms('+380956686191', 'test');
         $card_data = ClassType::whereIsset('description');
         return view('page.home', compact('card_data'));
     }
