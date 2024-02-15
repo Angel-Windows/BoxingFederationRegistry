c7b196cfbf453d1be0d373f3086e0a81e83e6d5f
Your Name <you@example.com>
2024-02-16 01:59:17 +0200

    Federation fix

--- resources/js/components/forms.js
+++ resources/js/components/forms.js
@@ -1,8 +1,7 @@
 import Inputmask from 'inputmask';
-const x = () => {
+const phone_input_mask = () => {
     const phoneInput = document.querySelector("input[name='phone']");
 
-    console.log(phoneInput);
     if (phoneInput) {
         const maskOptions = {
             mask: '+380 (99) 9999-999'
@@ -14,87 +13,91 @@ const x = () => {
 
 }
 export const inputs_input = (parent_class, is_search = true) => {
-    x()
+    phone_input_mask()
 
 
     let parent = document.querySelector('.' + parent_class);
     if (!parent) parent = document;
-    // console.log(parent)
     const all_inputs = parent.querySelectorAll('.input')
-    all_inputs.forEach((item) => {
-        item.addEventListener('focus', () => {
-            functionsArray['add_parent_active'](item, 'label', 'active')
+
+
+    if(all_inputs.length){
+        all_inputs.forEach((item) => {
+            item.addEventListener('focus', () => {
+                functionsArray['add_parent_active'](item, 'label', 'active')
+            })
+            item.addEventListener('blur', () => {
+                setTimeout(() => {
+                    if (item.value !== "") {
+                        functionsArray['add_parent_active'](item, 'label', 'active')
+                    } else {
+                        functionsArray['remove_parent_active'](item, 'label', 'active')
+                    }
+                }, 100)
+            })
         })
-        item.addEventListener('blur', () => {
-            setTimeout(() => {
-                if (item.value !== "") {
-                    functionsArray['add_parent_active'](item, 'label', 'active')
-                } else {
-                    functionsArray['remove_parent_active'](item, 'label', 'active')
+        const custom_select = parent.querySelectorAll('.custom-select')
+        custom_select.forEach((item) => {
+            let selectInput = item.querySelector('.custom-select-input');
+            let selectOptions = item.querySelector('.custom-select-options');
+            let optionItems = selectOptions.querySelectorAll('li');
+            let input_value = item.querySelector('.input-value')
+            let old_value = item.querySelector('.old-value')
+
+            selectInput.addEventListener('focus', function () {
+
+                old_value.value = selectInput.value;
+                if (is_search) {
+                    filterOptions(selectInput, selectOptions, optionItems);
                 }
-            }, 100)
-        })
-    })
-    const custom_select = parent.querySelectorAll('.custom-select')
-    custom_select.forEach((item) => {
-        let selectInput = item.querySelector('.custom-select-input');
-        let selectOptions = item.querySelector('.custom-select-options');
-        let optionItems = selectOptions.querySelectorAll('li');
-        let input_value = item.querySelector('.input-value')
-        let old_value = item.querySelector('.old-value')
+                selectOptions.style.display = 'block';
+            });
 
-        selectInput.addEventListener('focus', function () {
+            selectInput.addEventListener('input', function () {
+                if (is_search) {
+                    filterOptions(selectInput, selectOptions, optionItems);
+                }
+                selectOptions.style.display = 'block';
+            });
 
-            old_value.value = selectInput.value;
-            if (is_search) {
-                filterOptions(selectInput, selectOptions, optionItems);
-            }
-            selectOptions.style.display = 'block';
-        });
-
-        selectInput.addEventListener('input', function () {
-            if (is_search) {
-                filterOptions(selectInput, selectOptions, optionItems);
-            }
-            selectOptions.style.display = 'block';
-        });
-
-        selectOptions.addEventListener('click', function (e) {
-            if (e.target.tagName === 'LI') {
-                input_value.value = e.target.dataset.value
-                selectInput.value = e.target.textContent;
-                selectOptions.style.display = 'none';
-                item.classList.add('active')
-            }
-        });
-        parent.querySelector('form').addEventListener('click', function (e) {
-            if (!selectInput.contains(e.target) && !selectOptions.contains(e.target)) {
-                if (selectOptions.style.display === 'block') {
-                   if (selectInput.value !== ''){
-                       selectInput.value = old_value.value;
-                   }else {
-                       input_value.value = null;
-                       old_value.value = null;
-                   }
+            selectOptions.addEventListener('click', function (e) {
+                if (e.target.tagName === 'LI') {
+                    input_value.value = e.target.dataset.value
+                    selectInput.value = e.target.textContent;
                     selectOptions.style.display = 'none';
+                    item.classList.add('active')
                 }
-            }
-        });
-    })
+            });
+            parent.querySelector('form').addEventListener('click', function (e) {
+                if (!selectInput.contains(e.target) && !selectOptions.contains(e.target)) {
+                    if (selectOptions.style.display === 'block') {
+                        if (selectInput.value !== ''){
+                            selectInput.value = old_value.value;
+                        }else {
+                            input_value.value = null;
+                            old_value.value = null;
+                        }
+                        selectOptions.style.display = 'none';
+                    }
+                }
+            });
+        })
 
-    function filterOptions(selectInput, selectOptions, optionItems) {
-        let inputValue = selectInput.value.toLowerCase();
-        optionItems.forEach(function (item) {
-            if (inputValue === '') {
-                selectOptions.classList.add('show-all');
-            } else {
-                selectOptions.classList.remove('show-all');
-            }
-            if (item.textContent.toLowerCase().indexOf(inputValue) > -1) {
-                item.style.display = 'block';
-            } else {
-                item.style.display = 'none';
-            }
-        });
+        function filterOptions(selectInput, selectOptions, optionItems) {
+            let inputValue = selectInput.value.toLowerCase();
+            optionItems.forEach(function (item) {
+                if (inputValue === '') {
+                    selectOptions.classList.add('show-all');
+                } else {
+                    selectOptions.classList.remove('show-all');
+                }
+                if (item.textContent.toLowerCase().indexOf(inputValue) > -1) {
+                    item.style.display = 'block';
+                } else {
+                    item.style.display = 'none';
+                }
+            });
+        }
     }
+
 }
