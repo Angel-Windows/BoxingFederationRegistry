36150af0949a0014c2421afa7c9e73563dd093e1
Your Name <you@example.com>
2024-02-16 11:49:31 +0200

    Federation fix

--- resources/views/page/trainer_edit.blade.php
+++ resources/views/page/trainer_edit.blade.php
@@ -18,19 +18,33 @@
 
     <section class="nav">
         <h2>{{$get['more_data']['name']}}</h2>
+{{--@dd($class_name)--}}
         <div class="buttons">
-            <button onclick="functionsArray['ajax_postFormFind']('form_edit', 'form_edit_category')" class="button"><img
-                    src="{{asset('img/homeAbout/register.svg')}}" alt="register-icon">Зберегти
+            <a href="{{ route('page.class', [
+            'class_name' => $class_name,
+            'id' => $id
+        ]) }}" class="button">
+                <img
+                    src="{{asset('img/homeAbout/register.svg')}}"
+                    alt="register-icon"
+                >Відмінити
+            </a>
+            <button
+                onclick="functionsArray['ajax_postFormFind']('form_edit', 'form_edit_category')"
+                class="button"
+            ><img
+                    src="{{asset('img/icon/save.svg')}}" alt="register-icon">Зберегти
             </button>
         </div>
     </section>
     <section>
         <div class="">
-            <x-forms-category-register-form-component :class="$class_name" :id="$id" :get="$get" :type_submit="'register'">
-{{--                <div class="big_img">--}}
-{{--                    <div class="img"><img src="{{asset('img/users_img/9284da0c7ca70f123c97200aa73fa3dc.png')}}" alt="">--}}
-{{--                    </div>--}}
-{{--                </div>--}}
+            <x-forms-category-register-form-component :class="$class_name" :id="$id" :get="$get"
+                                                      :type_submit="'register'">
+                {{--                <div class="big_img">--}}
+                {{--                    <div class="img"><img src="{{asset('img/users_img/9284da0c7ca70f123c97200aa73fa3dc.png')}}" alt="">--}}
+                {{--                    </div>--}}
+                {{--                </div>--}}
                 @php
                     $class_img = '';
                     if ($get['more_data']['logo']){
@@ -43,12 +57,14 @@
                 <div class="upload_img {{$class_img}}" id="imageButton">
                     <div class="drop">Відпустити тут</div>
                     <div class="img">
-                        <input type="image" src="{{MyAsset($get['more_data']['logo']['link'] ?? '')}}" alt="" name="image">
+                        <input type="image" src="{{MyAsset($get['more_data']['logo']['link'] ?? '')}}" alt=""
+                               name="image">
                     </div>
                     <div class="buttons">
-{{--                        <button class="button one white">Зробити фото</button>--}}
-                        <button type="button" class="button one button_open_file white"><img src="{{asset('img/icon/edit.svg')}}" alt=""></button>
-                        <input type="file" name="photo"  style="display:none;">
+                        {{--                        <button class="button one white">Зробити фото</button>--}}
+                        <button type="button" class="button one button_open_file white"><img
+                                src="{{asset('img/icon/edit.svg')}}" alt=""></button>
+                        <input type="file" name="photo" style="display:none;">
                     </div>
                 </div>
             </x-forms-category-register-form-component>
